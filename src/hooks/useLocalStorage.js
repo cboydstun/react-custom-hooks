5f76d33bc8e6383bd35bce1e5b71d59e8db9c027
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-05-19 09:28:36 -0500

    fixed error

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -16,7 +16,7 @@ export default function useLocalStorage(key, initialValue) {
 
     useEffect(()=>{
         localStorage.setItem(key, JSON.stringify(value))
-    }, [value])
+    }, [key, value])
 
     return [value, setValue]
 }
