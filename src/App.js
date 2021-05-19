112ba4151439ecd55d84ea27105ac07bc8c2cee5
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2021-05-19 10:06:26 -0500

    currently broken

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import React from 'react'
 import './App.css'
 
 import DemoInput from './components/DemoInput'
+import DemoFetch from './components/DemoFetch'
 
 export default function App() {
     return (
         <div>
-            <DemoInput />
+            {/* <DemoInput /> */}
+            <DemoFetch />
         </div>
     )
 }
+
+//do demo input, then updatelogger, then local storage
